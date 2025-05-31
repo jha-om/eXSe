aed9b3e1749d03fb469a0aabdd187b3453874770
AUM <omjha50@gmail.com>
2025-06-01 03:32:03 +0530

    backend complete

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import "dotenv/config"
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
+import chatRoutes from "./routes/chat.route.js"
 import { connectDB } from "./db/index.js";
 import cookieParser from "cookie-parser"
 const app = express();
@@ -11,7 +12,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/usres", userRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port:${PORT}`); 
--- backend/src/utils/stream.js
+++ backend/src/utils/stream.js
@@ -18,4 +18,11 @@ export const upsertStreamUser = async (userData) => {
     }
 }
 
-export const generateStreamToken = (userId) => { };
+export const generateStreamToken = (userId) => {
+    try {
+        // const userIdStr = userId.toString();
+        return streamClient.createToken(userId.toString());
+    } catch (error) {
+        console.log("error while generating stream tokens", error.message);
+    }
+};
